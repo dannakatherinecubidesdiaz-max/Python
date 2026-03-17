41c766976d139e49814e45d152ecd42699068fbd
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-03-17 13:13:51 -0500

    Create 01_funciones4.py

---
+++ Python2/01_funciones4.py
@@ -0,0 +1,8 @@
+def calcular_area_rectangulo(base: float, altura: float) -> float:
+    """Calcula y retorna el resultado de la multiplicación de base y altura."""
+    resultado = base * altura
+    return resultado
+
+# Llamada a la función e impresión del resultado
+print(f"El área del rectángulo es: {calcular_area_rectangulo(5.0, 3.0)}")
+
